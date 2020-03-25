b59a89aed362e2d3066a3c07a191ccec97e0986f
djordje <me@mymail.com>
2020-03-25 18:32:10 +0000

    adding framework

--- src/main/java/com/javing/asclepius/services/CoordinatesFindingService.java
+++ src/main/java/com/javing/asclepius/services/CoordinatesFindingService.java
@@ -3,10 +3,24 @@ package com.javing.asclepius.services;
 import com.javing.asclepius.domain.Coordinates;
 import org.springframework.stereotype.Service;
 
+import static kong.unirest.Unirest.get;
+
 @Service
 public class CoordinatesFindingService {
 
     public Coordinates findCoordinatesFor(String address) {
+
+        //GET 28+Bisley+Cl,+Worcester+Park+KT4+8PN
+
+        String responseAsString = get(String.format("https://www.google.com/maps/place/%s", address
+                .replace(" ", "+")))
+                .asString().getBody();
+
+        int coordinateStart = responseAsString.indexOf("nter=");
+        int coordinateEnd = responseAsString.indexOf("&amp;zoom");
+
+        String substring = responseAsString.substring(coordinateStart, coordinateEnd);
+
         return new Coordinates(111D, 222D);
     }
 }
