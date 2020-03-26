39d719efd4fc44a94a298cf40d30352e29af1b15
djordje <me@mymail.com>
2020-03-26 15:26:27 +0000

    returning not found if unable to find ip

--- src/main/java/com/javing/asclepius/web/LocationsController.java
+++ src/main/java/com/javing/asclepius/web/LocationsController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 
+import javax.servlet.http.HttpServletRequest;
+
 import static com.javing.asclepius.services.IpFinder.getClientIp;
 import static org.springframework.web.context.request.RequestContextHolder.getRequestAttributes;
 
@@ -17,11 +19,14 @@ public class LocationsController {
     private final LocationsManagementService locationsManagementService;
 
     @RequestMapping(value = "/locations/new", consumes = "application/json", method = RequestMethod.POST)
-    public ResponseEntity<String> newLocation() {
+    public ResponseEntity<String> newLocation(HttpServletRequest request) {
+        String ipAddress = getClientIp((request));
+
+        if(ipAddress == null || ipAddress.equals("0:0:0:0:0:0:0:1"))
+            return ResponseEntity.notFound().build();
 
         return ResponseEntity.ok()
                 .body(locationsManagementService
-                        .newLocation(getClientIp(((ServletRequestAttributes) getRequestAttributes())
-                                .getRequest())));
+                        .newLocation(ipAddress));
     }
 }
