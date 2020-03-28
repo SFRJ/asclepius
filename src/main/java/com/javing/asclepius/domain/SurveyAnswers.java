40adb3121fd9f4288919fdb5e8b115a6ef6bf2f3
djordje <me@mymail.com>
2020-03-28 09:40:06 +0000

    rename

--- src/main/java/com/javing/asclepius/domain/SurveyAnswers.java
+++ src/main/java/com/javing/asclepius/domain/SurveyAnswers.java
@@ -8,7 +8,7 @@ public final class SurveyAnswers {
     private final Integer age;
     private final String sex;
     private final String status;
-    private final boolean chronic;
+    private final boolean isChronic;
     private final boolean isKeyWorker;
     private final boolean isIsolating;
 
