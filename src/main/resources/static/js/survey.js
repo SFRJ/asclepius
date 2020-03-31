5df34d987cbf82e5477e511bc8c69e103f61ed88
djordje <me@mymail.com>
2020-03-31 10:15:33 +0100

    fixing sintax errors

--- src/main/resources/static/js/survey.js
+++ src/main/resources/static/js/survey.js
@@ -5,20 +5,30 @@ var surveyDto = {
   sex:"",
   status:"",
   isChronic:"",
-  isKeyWorker:"",
-  isIsolating:""
+  isKeyWorker:""
 };
-
+//https://www.guru99.com/angularjs-ng-submit.html
 survey.controller('surveyController', function($scope, $http) {
 
-    $scope.submit = function() {
+    $scope.sendAnswers = function() {
 
         $http(
-            method: 'POST',
-            url: 'http://localhost:8080/add',
-            data: surveyDto,
-            headers: {'Content-Type': 'application/json;charset-UTF-8'}
-        );
-
+                {
+                    method: 'POST',
+                    url: 'http://localhost:8080/add',
+                    data: surveyDto,
+                    headers: {'Content-Type': 'application/json;charset-UTF-8'}
+                }
+        ).subscribe(
+                 (val) => {
+                     console.log("POST call successful value returned in body",
+                                 val);
+                 },
+                 response => {
+                     console.log("POST call in error", response);
+                 },
+                 () => {
+                     console.log("The POST observable is now completed.");
+                 });
     };
 });
